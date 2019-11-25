d01024e979fa8e0bfbac5ff8f2eb699923e8f98f
Milind Mevada <milind.mevada@solutelabs.com>
2019-11-25 11:13:11 +0530

    Initial commit. Scaffolding flutter package.

---
+++ test/horizontal_calendar_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:horizontal_calendar/horizontal_calendar.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  });
+}
